3820cabff8c4194954d6ed5ad279594e93be6ee7
Manish Sajwan <manish.sajwan@protonmail.com>
2023-05-14 16:23:34 +0530

    add btn

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -21,13 +21,14 @@ function App() {
         <button onClick={() => setCount((count) => count + 2)}>
           count is {count}
         </button>
+        
+        <button onClick={() => setCount((count) => count - 1)}>
+          count is {count}
+        </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
